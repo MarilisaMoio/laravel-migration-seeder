aa7fe21e609a8fc365567f8fc70008d9a7b0df9b
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-22 15:20:37 +0200

    feat: create table trains in db

--- database/migrations/2024_05_22_123941_create_trains_table.php
+++ database/migrations/2024_05_22_123941_create_trains_table.php
@@ -14,8 +14,28 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
+
             $table->id();
+            $table->string('operator', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('wagons');
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
             $table->timestamps();
+
+            //Azienda
+            //Stazione di partenza
+            //Stazione di arrivo
+            //Orario di partenza
+            //Orario di arrivo
+            //Codice Treno
+            //Numero Carrozze
+            //In orario
+            //Cancellato
         });
     }
 

36f07db28230409df7aa0a2342e25d77f0836a3f
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-22 15:48:26 +0200

    feat: add 10 trains to db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -15,6 +16,18 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for($i = 1; $i <= 10; $i++){
+            $train = new Train();
+            $train->operator = $faker->randomElement(['Trenitalia', 'Italo', 'Trenord']);
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->regexify('[A-Z0-9]{8}');
+            $train->wagons = $faker->numberBetween(4, 9);
+            $train->in_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
+            $train->save();
+        }
     }
 }
